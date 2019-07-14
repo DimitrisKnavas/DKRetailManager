79d956b427f3d5996da98f482fd34235e8efb80d
Δημήτρης <dimitriskarn94@gmail.com>
2019-07-14 19:30:00 +0300

    Add WPF Project with MVVM
    Adds our WPF user interface project and configures it with Caliburn Micro for our MVVM framework. Also includes the Simple Container DI system.

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        //handle the instantiation of almost all classes (dependency injection)
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        //where the instantiation happens....where does the container know what to connect to what
+        protected override void Configure()
+        {
+            _container.Instance(_container); //_container holds an instance of itself to pass when a simplecontainer is asked
+
+            //Singleton:create one instance of the class for the life of an application
+            _container
+                .Singleton<IWindowManager, WindowManager>()      //ties the interface to the implementation
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            //handle viewmodels to the views
+            GetType().Assembly.GetTypes()    //get every type in the entire application
+               .Where(type => type.IsClass)  //limit that where the type is a class..only class types
+               .Where(type => type.Name.EndsWith("ViewModel"))  //limit that where the name of the class ends with viewmodel
+               .ToList()   // we need to turn Ienum to list in order to use foreach
+               .ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
